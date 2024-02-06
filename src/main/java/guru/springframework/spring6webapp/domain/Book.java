6c4e88aad4f8cc88e062151c5e76eb77c4a32b2f
bryandj <bryandjw6@gmail.com>
2024-02-05 21:35:37 -0600

    Sección 2 Uso de entidades y relaciones APP

---
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -0,0 +1,50 @@
+package guru.springframework.spring6webapp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+    private String tittle;
+    private String isbn;
+    private Set<Book> books;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
